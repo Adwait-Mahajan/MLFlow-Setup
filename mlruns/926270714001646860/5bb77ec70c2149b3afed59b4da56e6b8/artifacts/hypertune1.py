6ec08d3739139fab1fd81bd37edbddec10ca5baa
Adwait <adwaitmahajan01@gmail.com>
2025-07-06 13:00:21 -0600

    MLOps Full pipeline done

--- mlruns/926270714001646860/5bb77ec70c2149b3afed59b4da56e6b8/artifacts/hypertune1.py
+++ mlruns/926270714001646860/5bb77ec70c2149b3afed59b4da56e6b8/artifacts/hypertune1.py
@@ -41,13 +41,6 @@ mlflow.set_experiment('breast-cancer-rf-hp')
 with mlflow.start_run() as parent:
     grid_search.fit(X_train, y_train)
 
-    #log all the child run
-    for i in range(len(grid_search.cv_results_['params'])):
-
-        with mlflow.start_run(nested=True) as child:
-            mlflow.log_params(grid_search.cv_results_['params'][i])
-            mlflow.log_metric("Accuracy", grid_search.cv_results_["mean_test_score"][i])
-
     # Displaying the best parameters and the best score
     best_params = grid_search.best_params_
     best_score = grid_search.best_score_
@@ -79,7 +72,7 @@ with mlflow.start_run() as parent:
     mlflow.sklearn.log_model(grid_search.best_estimator_, "random_forest")
 
     # Set tags
-    mlflow.set_tag("author", "Adwait Mahajan")
+    mlflow.set_tag("author", "Vikash Das")
 
     print(best_params)
     print(best_score)
